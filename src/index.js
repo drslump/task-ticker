41cdf75ce05c82bae3a097766dc13dad51776ab0
DrSlump <drslump@pollinimini.net>
2014-12-01 17:53:48 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,175 @@
+(function (exports) {
+
+    // Support node env
+    if (typeof require === 'function') {
+        var Promise = require('promise-es6').Promise;
+    }
+
+
+    function onValue (ticker, task, value) {
+        if (task.fulfilled) return;
+
+        task.fulfilled = true;
+        task.timeout && clearTimeout(task.timeout);
+        task.resolve(value);
+        return value;
+    }
+
+    function onError (ticker, task, error) {
+        if (task.fulfilled) return;
+
+        if (task.retry >= ticker.retries) {
+            task.fulfilled = true;
+            task.timeout && clearTimeout(task.timeout);
+            task.reject(error);
+        } else {
+            task.retry++;
+            setTimeout(
+                push.bind(null, ticker, task),
+                ticker.backoff(task.retry) * 1000
+            );
+        }
+    }
+
+    function process (ticker, task, expired) {
+        if (task.fulfilled) return;
+
+        if (expired) {
+            task.fulfilled = true;
+            task.reject(expired);
+            return;
+        }
+
+        try {
+            var result = task.callback(Date.now() - task.created);
+
+            // Apply logic to promises too
+            if (result && typeof result === 'object' && typeof result.then === 'function') {
+                result.then(
+                    onValue.bind(null, ticker, task),
+                    onError.bind(null, ticker, task)
+                );
+                return;
+            }
+            onValue(ticker, task, result);
+        } catch (e) {
+            onError(ticker, task, e);
+        }
+    }
+
+    function push (ticker, task) {
+        ticker.tasks.push(task);
+        tick(ticker);
+    }
+
+    function tick (ticker) {
+        if (ticker.paused || !ticker.tasks.length) return;
+
+        var now = Date.now(),
+            diff = now - ticker.timestamp,
+            min = ticker.interval * 1000;
+
+        if (diff < min) {
+            setTimeout(
+                tick.bind(null, ticker),
+                min - diff
+            );
+            return;
+        }
+
+        var task = ticker.tasks.shift();
+        process(ticker, task);
+
+        ticker.timestamp = now;
+        tick(ticker);
+    }
+
+    function timeout (ticker, task) {
+        if (!ticker.timeout) return;
+        task.timeout = setTimeout(function () {
+            var idx = ticker.tasks.indexOf(task);
+            if (-1 !== idx) {
+                ticker.tasks.splice(idx, 1);
+            }
+            process(ticker, task, TaskTicker.EXPIRED_ERROR);
+        }, ticker.timeout * 1000);
+    }
+
+
+    function TaskTicker (int_or_opts) {
+
+        if (!(this instanceof TaskTicker)) {
+            return new TaskTicker(int_or_opts);
+        }
+
+        var opts = typeof int_or_opts !== 'object'
+                 ? { interval: int_or_opts }
+                 : int_or_opts;
+
+        this.paused = 0;
+        this.interval = opts.interval === 0 ? 0 : opts.interval || 0.016;
+        this.timeout = opts.timeout || null;
+        this.retries = opts.retries || 0,
+        this.backoff = typeof opts.backoff === 'function'
+                     ? opts.backoff
+                     : function (n) {
+                        if (opts.backoff === 0) return 0;
+                        return ((opts.backoff || 2) << n) * (this.interval || 0.010)
+                     }.bind(this);
+
+        this.tasks = [];
+        this.timestamp = 0;
+    }
+    TaskTicker.prototype = Object.create(null);
+    TaskTicker.prototype.constructor = TaskTicker;
+
+    TaskTicker.prototype.queue = function (cb) {
+        var ticker = this;
+        return new TaskTicker.Promise(function (resolve, reject) {
+            var task = {
+                resolve: resolve,
+                reject: reject,
+
+                fulfilled: false,
+                timeout: null,
+                created: Date.now(),
+                retry: 0,
+                callback: typeof cb === 'function' ? cb : function () { return cb; }
+            };
+
+            timeout(ticker, task);
+            push(ticker, task);
+        });
+    };
+
+    TaskTicker.prototype.pause = function () {
+        this.paused = true;
+    };
+
+    TaskTicker.prototype.resume = function () {
+        this.paused = false;
+        tick(this);
+    };
+
+    TaskTicker.prototype.reset = function () {
+        var task;
+        while (this.tasks.length) {
+            task = this.tasks.shift();
+            process(this, task, TaskTicker.RESET_ERROR);
+        }
+    };
+
+
+    TaskTicker.Error = function (msg) { this.message = '' + msg; };
+    TaskTicker.Error.prototype = Object.create(Error.prototype);
+    TaskTicker.Error.prototype.constructor = TaskTicker.Error;
+
+    TaskTicker.EXPIRED_ERROR = new TaskTicker.Error('Timeout');
+    TaskTicker.RESET_ERROR = new TaskTicker.Error('Reset');
+
+    TaskTicker.Promise = typeof Promise !== undefined ? Promise : null;
+
+
+    exports.TaskTicker = TaskTicker;
+
+})(this);
---
+++ tests/task-ticker.spec.js
@@ -0,0 +1,249 @@
+describe('TaskTicker', function () {
+    var sinon = require('sinon');
+    var should = require('chai').should();
+
+    // Reduce mocha's default timeout
+    this.timeout(5000);
+
+
+    var TaskTicker = require('../src').TaskTicker;
+
+    var Promise = require('promise-es6').Promise;
+
+
+    var clock, spy;
+    beforeEach(function () {
+        clock = sinon.useFakeTimers(3600000);
+        spy = sinon.spy();
+    });
+
+    afterEach(function () {
+        clock.restore();
+    });
+
+    describe('Simple', function () {
+
+        var tt;
+        beforeEach(function () {
+            tt = TaskTicker(0.1);
+        });
+
+        it('should execute first task immediately', function () {
+            tt.queue(spy);
+            spy.callCount.should.equal(1);
+        });
+
+        it('should wait to execute second task', function () {
+            tt.queue(spy);
+            tt.queue(spy);
+
+            spy.callCount.should.equal(1);
+            clock.tick(50);
+            spy.callCount.should.equal(1);
+            clock.tick(50);
+            spy.callCount.should.equal(2);
+        });
+
+        it('should execute second immediately if interval passed by', function (){
+            tt.queue(spy);
+            spy.callCount.should.equal(1);
+            clock.tick(200);
+            tt.queue(spy);
+            spy.callCount.should.equal(2);
+        });
+
+        it('should provide elapsed time', function (done) {
+            tt.queue(spy);
+            tt.queue(function (elapsed) {
+                elapsed.should.be.above(100 - 1);
+                done();
+            });
+            clock.tick(100);
+            clock.restore();
+        });
+
+        it('should resolve promise', function (done) {
+            tt.queue('foo')
+            .then(function (x) {
+                x.should.equal('foo');
+                done();
+            });
+            clock.tick();
+            clock.restore();
+        });
+
+        it('should reject promise', function (done) {
+            tt.queue(function () {
+                throw new Error('foo');
+            })
+            .catch(function (err) {
+                err.message.should.equal('foo');
+                done();
+            })
+            clock.tick();
+            clock.restore();
+        });
+
+        it('should not defer when interval=0', function () {
+            tt = TaskTicker(0);
+            tt.queue(spy);
+            tt.queue(spy);
+            spy.callCount.should.equal(2);
+        });
+
+        it('should pause', function () {
+            tt.queue(spy);
+            tt.queue(spy);
+            tt.queue(spy);
+
+            spy.callCount.should.equal(1);
+
+            tt.pause();
+
+            clock.tick(1000);
+            spy.callCount.should.equal(1);
+
+            tt.resume();
+            spy.callCount.should.equal(2);
+
+            clock.tick(100);
+            spy.callCount.should.equal(3);
+        });
+
+    });
+
+    describe('Promise', function () {
+
+        var tt;
+        beforeEach(function () {
+            tt = TaskTicker(0.1);
+        });
+
+        it('should wait for promise resolution', function (done) {
+            tt.queue(function () {
+                return new Promise(function (resolve, reject) {
+                    setTimeout(resolve.bind(null, 'foo'), 1000);
+                });
+            })
+            .then(function (data) {
+                spy();
+                data.should.equal('foo');
+                done();
+            });
+
+            spy.callCount.should.equal(0);
+            clock.tick(200);
+            spy.callCount.should.equal(0);
+            clock.tick(1000);
+            spy.callCount.should.equal(1);
+
+            clock.restore();
+        });
+
+        it('should wait for promise rejection', function (done) {
+            tt.queue(function () {
+                return new Promise(function (resolve, reject) {
+                    setTimeout(reject.bind(null, Error('foo')), 1000);
+                });
+            })
+            .catch(function (error) {
+                spy();
+                error.message.should.equal('foo');
+                done();
+            });
+
+            spy.callCount.should.equal(0);
+            clock.tick(200);
+            spy.callCount.should.equal(0);
+            clock.tick(1000);
+            spy.callCount.should.equal(1);
+
+            clock.restore();
+        });
+    });
+
+
+    describe('Timeout', function () {
+        var tt;
+        beforeEach(function () {
+            tt = TaskTicker({
+                interval: 0.1,
+                timeout: 0.20
+            });
+        });
+
+        it('should reject with a timeout', function (done) {
+            tt.queue(spy);
+            tt.queue(spy);
+
+            tt.queue(spy)
+            .catch(function (err) {
+                err.should.be.instanceof(TaskTicker.Error);
+                done();
+            });
+
+            clock.tick(200);
+            clock.restore();
+
+            spy.callCount.should.equal(2);
+        });
+
+        it('should reject with a timeout waiting for promise', function (done) {
+            tt.queue(function () {
+                return new Promise(function (resolve, reject) {
+                    setTimeout(resolve.bind(null, 'foo'), 250);
+                });
+            })
+            .catch(function (err) {
+                err.should.be.instanceof(TaskTicker.Error);
+                done();
+            });
+
+            clock.tick(200);
+            clock.restore();
+        })
+    });
+
+    describe('Retries', function () {
+        var tt;
+        beforeEach(function () {
+            tt = TaskTicker({
+                interval: 0.1,
+                retries: 2,
+                backoff: 0,
+                timeout: 2.0
+            });
+        });
+
+        it('should retry upto a maximum', function (done) {
+            tt.queue(function () {
+                spy();
+                throw new Error('foo');
+            })
+            .catch(function (err) {
+                err.message.should.equal('foo');
+                done();
+            });
+
+            clock.tick(500);
+            spy.callCount.should.equal(3);
+        });
+
+        it('should resolve after retry', function (done) {
+            tt.queue(function () {
+                spy();
+                if (spy.callCount == 2) return 'foo';
+                throw new Error('foo');
+            })
+            .then(function (data) {
+                data.should.equal('foo');
+                done();
+            });
+
+            clock.tick(500);
+            spy.callCount.should.equal(2);
+        });
+
+    });
+
+});
